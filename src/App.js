2720eb2b458bdc3ed73583f4838dc9342afecdc7
Camila Wheeler <camila_wheeler@outlook.com>
2022-08-24 10:53:34 -0700

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
@@ -14,6 +15,10 @@ function App() {
                 <Route path='/' element={<HomePage />} />
                 <Route path='contact' element={<ContactPage />} />
                 <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
             </Routes>
             <Footer />
         </div>
